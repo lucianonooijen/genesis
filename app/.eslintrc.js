8924f3d62a3db5678ac63d0667208242094461c1
Luciano Nooijen <luciano@bytecode.nl>
2021-12-20 16:57:52 +0200

    Fix import paths and add import checks in Eslint
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -20,22 +20,23 @@ module.exports = {
     settings: {
         "import/resolver": {
             node: {
-                paths: ["src"],
                 extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
             },
+            typescript: {
+                project: "./tsconfig.json"
+            }
         },
     },
-    rules: { // In alphabetical order
-        "import/extensions": ["error", "never"],
-        "import/no-unresolved": 0,
-        "import/prefer-default-export": 0,
+    rules: { // In alphabetical order, all rules require a comment to explain the change and necessity
+        "import/extensions": 0, // The Typescript compiler will check imports extensions
+        "import/prefer-default-export": 0, // Allow 'export const' exports
         "jsx-a11y/accessible-emoji": "off", // Allow use of Emojis
-        "no-shadow": 0, // For allowing headacheless Redux Action in props
+        "no-shadow": 0, // For allowing headacheless Redux Action in props in JS code
         "no-use-before-define": 0, // Allow sane file layouts
-        "react/function-component-definition": [1, { namedComponents: "arrow-function", unnamedComponents: "arrow-function" }],
-        "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+        "react/function-component-definition": [1, { namedComponents: "arrow-function", unnamedComponents: "arrow-function" }], // Force arrow function
+        "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }], // Force files to have .jsx or .tsx
         "react/prop-types": 0, // Done by using React.FC<PropsInterface>
-        "@typescript-eslint/no-shadow": ["error"],
+        "@typescript-eslint/no-shadow": ["error"], //  For allowing headacheless Redux Action in props in TS code
         "@typescript-eslint/no-use-before-define": 0, // Allow sane file layouts
     },
     overrides: [
--- app/App.test.tsx
+++ app/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {render} from "@testing-library/react-native";
-import App from "./app";
+import App from "./App";
 
 describe("app", () => {
     it("should render without throwing", () => {
