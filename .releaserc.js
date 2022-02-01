1d331e7c27552e771e505478ffdc7b39ec05105f
Luciano Nooijen <luciano@bytecode.nl>
2022-02-01 15:37:05 +0200

    ci: add commitlint
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ .releaserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    branches: ["master", "cicd"],
+    plugins: [
+        ["@semantic-release/commit-analyzer", {
+            preset: "conventionalcommits",
+            releaseRules: [
+                {breaking: true, release: "major"},
+                {revert: true, release: "patch"},
+                {type: "feat", release: "minor"},
+                {type: "fix", release: "patch"},
+                {type: "perf", release: "patch"},
+                {type: "build", release: "patch"},
+                {type: "ci", release: "patch"},
+                {type: "docs", release: "patch"}
+            ]
+        }],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/gitlab",
+        ["@semantic-release/git", {
+            message: "release: ${nextRelease.version}\n\n${nextRelease.notes}",
+            assets: ["CHANGELOG.md"]
+        }]
+    ]
+}
---
+++ docs/commits.md
@@ -0,0 +1,38 @@
+# Commit rules
+
+Commit names are used to automatically generate changelogs. Therefore, commit titles must conform to a specific format.
+
+The format used is:
+
+```
+type(optional scope): details
+```
+
+For example
+
+```
+feat(app): add jwt refresh if the token is valid for less than a week
+```
+
+## Types and scopes
+
+The possible types are:
+
+- build
+- ci
+- docs
+- feat
+- fix
+- perf
+- refactor
+- release
+- revert
+- style
+- test
+
+The possible scopes are:
+
+- app
+- docs
+- init
+- server
