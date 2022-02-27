4be9c46e0ae39aa80a6f3ff6271827cc8c699fc7
Luciano Nooijen <luciano@bytecode.nl>
2022-02-27 10:32:27 +0200

    fix: set correct version number (was outdated)
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- server/internal/constants/version.go
+++ server/internal/constants/version.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	// APIVersion is the version of the API.
-	APIVersion = "0.1.0"
+	APIVersion = "0.1.1"
 
 	// MinimumClientVersion is the minimum version of the app, requests from older clients will be rejected by the server.
 	MinimumClientVersion = "0.0.1"
